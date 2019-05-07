4f3a5ee5222af59c7bf6b6d6f3e550769ab7a4c1
Fagner Fernandes <ffcs@cin.ufpe.br>
2019-05-07 11:16:29 -0300

    Import inicial.

---
+++ src/test/java/br/cin/ufpe/sdpos/mpback/MpbackApplicationTests.java
@@ -0,0 +1,16 @@
+package br.cin.ufpe.sdpos.mpback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MpbackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
