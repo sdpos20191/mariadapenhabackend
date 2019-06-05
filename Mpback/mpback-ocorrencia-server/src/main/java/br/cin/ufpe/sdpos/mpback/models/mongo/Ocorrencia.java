a9fbf4b7ecd4294fb2ac462e0960c73d374be18b
Daniel da Cruz Brandão <dcb@cin.ufpe.br>
2019-06-05 01:23:52 -0300

    Ajuste no nome da collection

--- Mpback/mpback-ocorrencia-server/src/main/java/br/cin/ufpe/sdpos/mpback/models/mongo/Ocorrencia.java
+++ Mpback/mpback-ocorrencia-server/src/main/java/br/cin/ufpe/sdpos/mpback/models/mongo/Ocorrencia.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.List;
 
-@Document(collection = "ocrrencias")
+@Document(collection = "ocorrencias")
 public @Data class Ocorrencia {
 
     @Id
