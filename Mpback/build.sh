b8358201bdf1d964be298181fe1b2ff1d50a5d0a
Daniel da Cruz Brandão <dcb@cin.ufpe.br>
2019-06-05 01:25:41 -0300

    Adicionado script para efetuar build em todos os Dockerfiles em sequência

---
+++ Mpback/build.sh
@@ -0,0 +1,4 @@
+docker build -t mpback_config ./mpback-config-server
+docker build -t mpback_eureka ./mpback-eureka-server
+docker build -t mpback_gateway ./mpback-gateway-server
+docker build -t mpback_ocorrencia-server ./mpback-ocorrencia-server
