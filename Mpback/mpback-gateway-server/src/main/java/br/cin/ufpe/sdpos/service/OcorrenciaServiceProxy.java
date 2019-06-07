dd44c7fbbb94bfba9cb030d819ab97d8aa148e0c
leandrooguit <leandrooguit@gmail.com>
2019-06-07 04:54:27 -0300

    ajuste na configuração de cada microservices que não estava executando

--- Mpback/mpback-gateway-server/src/main/java/br/cin/ufpe/sdpos/service/OcorrenciaServiceProxy.java
+++ Mpback/mpback-gateway-server/src/main/java/br/cin/ufpe/sdpos/service/OcorrenciaServiceProxy.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.cin.ufpe.sdpos.model.OcorrenciaDtoInfo;
 
-@FeignClient(name = "mpback-ocorrencia-server", url = "http://ocorrencia-server:9093")
+@FeignClient(name = "ocorrencia-server", url = "http://ocorrencia-server:9093")
 public interface OcorrenciaServiceProxy {
 
 	@GetMapping(value = "/api/v1/jpa/ocorrencias")
