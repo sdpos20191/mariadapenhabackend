0b94c0cf6843d4ce4cb59b526274d3dcabd7c401
Daniel da Cruz Brandão <dcb@cin.ufpe.br>
2019-06-11 12:01:12 -0300

    Ordenação dos containers alteradas de tempo para verificação de status

--- Mpback/mpback-eureka-server/sleepEntry.sh
+++ Mpback/mpback-eureka-server/sleepEntry.sh
@@ -1,4 +1,32 @@
-echo Aguardando $1 segundos
-sleep $1
+#!/bin/sh
+
+webserv=$1
+keyword=$2
+stat="error"
+
+available () {
+        echo "Testing availability"
+        if curl -s "$webserv" | grep "$keyword"
+        then
+            # if the keyword is in the conent
+            echo $webserv "is working fine"
+            stat="ok"
+        else
+            echo $webserv "still not up"
+        fi
+}
+
+echo Aguardando $webserv;
+
+available
+
+while [ $stat != "ok" ]; do
+
+        sleep 1
+        available
+
+done
+
 echo Iniciando ...
-java -jar $2
+                             
+java -jar $3
